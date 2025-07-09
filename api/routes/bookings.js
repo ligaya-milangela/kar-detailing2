90b9656aed0b18896433047be9307c8e54aa9004
ligaya-milangela <jodacuba@gbox.adnu.edu.ph>
2025-07-10 01:27:29 +0800

    token1

--- api/routes/bookings.js
+++ api/routes/bookings.js
@@ -9,10 +9,12 @@ const JWT_SECRET = process.env.JWT_SECRET || "secret123";
 // Middleware: Authenticate using JWT token from cookies
 function auth(req, res, next) {
     
-  const token = req.cookies.token;
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ error: "Missing token" });
+
+  const token = authHeader.split(" ")[1];
   if (!token) return res.status(401).json({ error: 'No token' });
   try {
-    req.user = jwt.verify(token, JWT_SECRET);
     next();
   } catch (err) {
     res.status(401).json({ error: 'Invalid token' });
